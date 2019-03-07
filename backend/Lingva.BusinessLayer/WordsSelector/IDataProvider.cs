853366ace0a6e3322fbf06b36efa4829c44cd080
Konstantin <t676526253@gmail.com>
2019-03-07 12:40:39 +0200

    add words selector from the subtitles

---
+++ backend/Lingva.BusinessLayer/WordsSelector/IDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lingva.BusinessLayer.WordsSelector
+{
+    public interface IDataProvider : IDisposable
+    {
+        List<string> GetAllSimpleWords();
+        bool AreExist(string word);
+        List<string> GetUserDictionary(int userId);
+    }
+}

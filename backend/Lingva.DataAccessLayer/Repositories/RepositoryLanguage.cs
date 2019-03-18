fdeebf6d15780b3af316c12d1e7898e059689e48
HannaPluita <hannapluita@gmail.com>
2019-03-18 20:46:54 +0200

    Fixing bugs

---
+++ backend/Lingva.DataAccessLayer/Repositories/RepositoryLanguage.cs
@@ -0,0 +1,16 @@
+﻿using Lingva.DataAccessLayer.Context;
+using Lingva.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lingva.DataAccessLayer.Repositories
+{
+    public class RepositoryLanguage: Repository<Language>, IRepositoryLanguage
+    {
+        public RepositoryLanguage(DictionaryContext context)
+            : base(context)
+        {
+        }
+    }
+}

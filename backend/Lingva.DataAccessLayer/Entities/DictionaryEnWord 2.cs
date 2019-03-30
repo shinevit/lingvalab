8ca904500a285d8093b1be902bbeba3276d53f7e
Maksym Isaiev <isaev140295@gmail.com>
2019-03-30 14:45:16 +0200

    Merged Statistics

---
+++ backend/Lingva.DataAccessLayer/Entities/DictionaryEnWord 2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Lingva.DataAccessLayer.Entities
+{
+    public class DictionaryEnWord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}

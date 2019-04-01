d92d031878231e2aa07e82d25e27e8304003046f
vitalii-radchenko <vitalii.radchenko@bd.com>
2019-04-01 18:34:33 +0300

    Deleted prepare.docker.sh. Moved one to docker-deploy.sh

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-
+# create container and start app server
 docker run --restart=unless-stopped -p 5000:80 -d --name lingva-app lingva-img
+
+# set the connectionString as environment variable
+
+# deploy a seed data optional
+
+# restart the container
+
